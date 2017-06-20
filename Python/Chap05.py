b80b6a72600f8daf2566d02e906842ed361e05d1
clj <474691391@qq.com>
2017-06-20 10:07:31 +0800

    Initial commit

---
+++ Python/Chap05.py
@@ -0,0 +1,9 @@
+words=['this','is','an','ex','parrot']
+for word in words:
+    print word
+
+names=['anna','beth','george','damon']
+ages=[12,45,23,102]
+for i in range(len(names)):
+    print names[i],'is',ages[i]
+    #'print names[i],'is',ages[i]
---
+++ TensorflowTest/helloworld.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print sess.run(hello)
+
+a = tf.constant(10)
+b = tf.constant(32)
+print sess.run(a + b)
+matrix1=tf.constant([[3.,3.]])
+matrix2=tf.constant([[2.],[2.]])
+product=tf.matmul(matrix1,matrix2)
+sess=tf.Session()
